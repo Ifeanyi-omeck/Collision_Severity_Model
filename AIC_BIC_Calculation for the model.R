c397a9275a3db87b97fdcc30eb7ba1aa332b2da2
Ifeanyi Anthony Omeifejideofor <57332609+Ifeanyi-omeck@users.noreply.github.com>
2023-07-08 20:35:09 +0100

    Adding function for Calculating AIC and BIC

---
+++ AIC_BIC_Calculation for the model.R
@@ -0,0 +1,20 @@
+# Function to calculate AIC and BIC for a custom zero-inflated model
+
+calculate_AIC_BIC <- function(model, n) {
+  
+  # Model: Pass Custom Zero-inflated model component
+  # n: Total number of observations in the model
+  
+  nll <- model$ll  # Negative log-likelihood
+  num_parameters <- sum(sapply(model$coefficients, length))  # Number of estimated parameters
+  
+  AIC <- -2 * nll + 2 * num_parameters 
+  BIC <- -2 * nll + log(n) * num_parameters 
+  
+  return(list(AIC = AIC, BIC = BIC))
+}
+
+# Usage example:
+estimates <- calculate_AIC_BIC(model_ziop, n = 9624)
+estimates$AIC
+estimates$BIC
