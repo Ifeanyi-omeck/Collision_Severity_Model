66c5f9c8e229d378aaffa1a906ddd3bcc79efbb6
Ifeanyi Anthony Omeifejideofor <57332609+Ifeanyi-omeck@users.noreply.github.com>
2023-07-20 19:56:45 +0100

    Making changes to the code

--- marginal_effect_zero_inflated_model.R
+++ marginal_effect_zero_inflated_model.R
@@ -21,50 +21,39 @@ ziop_marginal_effects <- function(first_mod) {
   # This is a common approach in calculating marginal effects.
   
   # Extract Estimates from the model
-  cat_y_variable <- unique(first_mod$y)
-  lens <- length(cat_y_variable)
-  column_names <- colnames(first_mod$x)
-  x_var <- first_mod$x
-  form <- paste("~ 1", paste("+", column_names, collapse = " "), collapse = " ")
-  x_lin <- model.matrix(as.formula(form), data = x_var)[, -1]
-  x_lin_bar <- as.matrix(colMeans(x_lin))
-  model_estimates <- first_mod$coefficients$ordered
-  model_estimates_new <- unname(model_estimates)
-  len_model_estimates <- nrow(summary(first_mod)$coefficients[-c(1,2), ])
-  xb <- t(x_lin_bar) %*% model_estimates
+  cat_y_variable <- unique(first_mod$y)  # Extract unique values of the dependent variable
+  lens <- length(cat_y_variable)  # Determine the number of unique values
+  column_names <- colnames(first_mod$x)  # Get the column names of the independent variables
+  x_var <- first_mod$x  # Assign the independent variables to a new variable
+  form <- paste("~ 1", paste("+", column_names, collapse = " "), collapse = " ")  # Create the formula for the model
+  x_lin <- model.matrix(as.formula(form), data = x_var)[, -1]  # Generate the design matrix for the model
+  x_lin_bar <- as.matrix(colMeans(x_lin))  # Calculate the column means of the design matrix
+  model_estimates <- first_mod$coefficients$ordered  # Extract the model estimates
+  model_estimates_new <- unname(model_estimates)  # Remove the names from the model estimates
+  len_model_estimates <- nrow(summary(first_mod)$coefficients[-c(1,2), ])  # Determine the number of model estimates
+  xb <- t(x_lin_bar) %*% model_estimates  # Calculate the linear predictors
   
   # Setting lower Bound Threshold for the cut-points
-  z <-  summary(first_mod)$coefficients[grep("^cut", rownames(summary(first_mod)$coefficients)), "Estimate"]
-  z <- c(-10^6, z, 10^6)
+  z <-  summary(first_mod)$coefficients[grep("^cut", rownames(summary(first_mod)$coefficients)), "Estimate"]  # Extract the cut-point estimates
+  z <- c(-10^6, z, 10^6)  # Set lower and upper bounds for the cut-points
   
-  # Instantiating Probit Function
-  dist_function <- dnorm
-  prob_function <- pnorm
-  
-  # Obtaining the covariance matrix of the model
-  first_cov <- vcov(first_mod)
-  second_cov <- rbind(cbind(first_cov, 0, 0), 0, 0)
-  ind <- c(1:len_model_estimates, nrow(second_cov)-1, (len_model_estimates+1):(len_model_estimates+lens-1), nrow(second_cov))
-  third_cov <- second_cov[ind,]
-  third_cov <- second_cov[, ind]
+  # Instantiating Function
+  dist_function <- dnorm  # Specify the distribution function to use (in this case, the standard normal distribution)
   
   # Calculating the Marginal Effects
-  effects.xb <- dist_function(z[1:lens] - c(xb)) -dist_function(z[2:(lens + 1)] -c(xb))
-  marg_effects <- model_estimates %*% matrix(data =  effects.xb, nrow = 1)
+  effects.xb <- dist_function(z[1:lens] - c(xb)) - dist_function(z[2:(lens + 1)] - c(xb))  # Calculate the marginal effects
+  marg_effects <- model_estimates %*% matrix(data =  effects.xb, nrow = 1)  # Multiply the model estimates by the marginal effects
   
   # Passing variables to a data frame
-  marg_effects_df <- as.data.frame(marg_effects)
+  marg_effects_df <- as.data.frame(marg_effects)  # Convert the marginal effects to a data frame
   
   # Assign column names as row names
-  column_names <- gsub("^X", "", column_names)  # Remove "X" from row names
-  row.names(marg_effects_df) <- column_names
+  column_names <- gsub("^X", "", column_names)  # Remove "X" from the column names
+  row.names(marg_effects_df) <- column_names  # Assign the column names as row names
   
   # Renaming the Marginal effect Column data frame
-  col_names <- paste("marginal_effect", 1:ncol(marg_effects_df))
-  colnames(marg_effects_df) <- col_names
+  col_names <- paste("marginal_effect", 1:ncol(marg_effects_df))  # Generate column names for the data frame
+  colnames(marg_effects_df) <- col_names  # Assign the column names to the data frame
   
-  return(marg_effects_df)
+  return(marg_effects_df)  # Return the data frame of marginal effects
 }
-
-
-ziop_marginal_effects(model_ziop)
