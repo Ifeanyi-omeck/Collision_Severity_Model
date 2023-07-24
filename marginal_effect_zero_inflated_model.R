4a4bf3c43ce4136ee3c7147004a2358552b2b4ca
Ifeanyi Anthony Omeifejideofor <57332609+Ifeanyi-omeck@users.noreply.github.com>
2023-07-24 20:33:45 +0100

    Fixing the code

--- marginal_effect_zero_inflated_model.R
+++ marginal_effect_zero_inflated_model.R
@@ -1,12 +1,12 @@
-ziop_marginal_effects <- function(first_mod) {
+ziop_marginal_effects <- function(ZIOP_model) {
   
-  # dist_function is a reference to the standard normal probability density function, dnorm. 
+  # dnorm is a reference to the standard normal probability density function. 
   # This function is used to calculate the difference in the density of the ordered categories 
   # (i.e., the difference in the density of the probability of being in one category versus the next).
   
-  # The operations z[1:lens] - c(xb) and  z[2:(lens + 1)] - c(xb)
-  # represent the calculation of the z-scores for each category. 
-  # Here, lens refers to the number of categories in the response variable, 
+  # The operations uj[1:len_response_variable] - c(xb) and  uj[2:(len_response_variable + 1)] - c(xb)
+  # represent the calculation of the threshold values  for each category. 
+  # Here, len_response_variable refers to the number of categories in the response variable, 
   # and xb represents the predicted values on the linear scale (the X?? part of the model).
   
   # After calculating the differences in densities, 
@@ -21,39 +21,64 @@ ziop_marginal_effects <- function(first_mod) {
   # This is a common approach in calculating marginal effects.
   
   # Extract Estimates from the model
-  cat_y_variable <- unique(first_mod$y)  # Extract unique values of the dependent variable
-  lens <- length(cat_y_variable)  # Determine the number of unique values
-  column_names <- colnames(first_mod$x)  # Get the column names of the independent variables
-  x_var <- first_mod$x  # Assign the independent variables to a new variable
-  form <- paste("~ 1", paste("+", column_names, collapse = " "), collapse = " ")  # Create the formula for the model
-  x_lin <- model.matrix(as.formula(form), data = x_var)[, -1]  # Generate the design matrix for the model
-  x_lin_bar <- as.matrix(colMeans(x_lin))  # Calculate the column means of the design matrix
-  model_estimates <- first_mod$coefficients$ordered  # Extract the model estimates
-  model_estimates_new <- unname(model_estimates)  # Remove the names from the model estimates
-  len_model_estimates <- nrow(summary(first_mod)$coefficients[-c(1,2), ])  # Determine the number of model estimates
-  xb <- t(x_lin_bar) %*% model_estimates  # Calculate the linear predictors
+  response_variable <- unique(ZIOP_model$y)  # Grabbing the unique values in response variable from the ZIOP model.
   
-  # Setting lower Bound Threshold for the cut-points
-  z <-  summary(first_mod)$coefficients[grep("^cut", rownames(summary(first_mod)$coefficients)), "Estimate"]  # Extract the cut-point estimates
-  z <- c(-10^6, z, 10^6)  # Set lower and upper bounds for the cut-points
+  len_response_variable  <- length(response_variable)  # Determine the length of Unique.
   
-  # Instantiating Function
-  dist_function <- dnorm  # Specify the distribution function to use (in this case, the standard normal distribution)
+  column_names <- colnames(ZIOP_model$x)  # Get the column names of the independent variables.
+  
+  x_variables <- ZIOP_model$x  # Assign the independent variables to a new variable.
+  
+  
+  # Create the formula for the model
+  form <- paste("~ 1", paste("+", column_names, collapse = " "), collapse = " ") 
+  
+  # Generate the design matrix for the model. Excluding the intercept.
+  x_lin <- model.matrix(as.formula(form), data = x_variables)[, -1] 
+  
+  # Calculate the mean of the design matrix.
+  x_lin_bar <- as.matrix(colMeans(x_lin)) 
+  
+  # Extract the model estimates from the Ordered components.
+  model_estimates <- ZIOP_model$coefficients$ordered  
+  
+  # Remove the names from the model estimates
+  model_estimates_new <- unname(model_estimates)  
+  
+  
+  # Calculate the linear predictors.
+  xb <- t(x_lin_bar) %*% model_estimates  
+  
+  
+  # Setting lower Bound Threshold for the cut-points.
+  # Extract the cut-point estimates.
+  uj <-  summary(ZIOP_model)$coefficients[grep("^cut", rownames(summary(ZIOP_model)$coefficients)), "Estimate"]  
+  uj <- c(-10^6, uj, 10^6)  # Set lower and upper bounds for the cut-points
+  
   
   # Calculating the Marginal Effects
-  effects.xb <- dist_function(z[1:lens] - c(xb)) - dist_function(z[2:(lens + 1)] - c(xb))  # Calculate the marginal effects
-  marg_effects <- model_estimates %*% matrix(data =  effects.xb, nrow = 1)  # Multiply the model estimates by the marginal effects
+  effects.xb <- dnorm(uj[1:len_response_variable] - c(xb)) - dnorm(uj[2:(len_response_variable + 1)] - c(xb))
   
-  # Passing variables to a data frame
-  marg_effects_df <- as.data.frame(marg_effects)  # Convert the marginal effects to a data frame
+  # Multiply the model estimates by the marginal effects
+  marg_effects <- model_estimates %*% matrix(data =  effects.xb, nrow = 1)  
   
-  # Assign column names as row names
-  column_names <- gsub("^X", "", column_names)  # Remove "X" from the column names
-  row.names(marg_effects_df) <- column_names  # Assign the column names as row names
+  # Passing variables to a data frame.
+  marg_effects_df <- as.data.frame(marg_effects)
   
-  # Renaming the Marginal effect Column data frame
-  col_names <- paste("marginal_effect", 1:ncol(marg_effects_df))  # Generate column names for the data frame
+  # Assign column names as row names.
+  # Remove "X" from the column names.
+  column_names <- gsub("^X", "", column_names) 
+  
+  # Assign the column names as row names.
+  row.names(marg_effects_df) <- column_names  
+  
+  # Renaming the Marginal effect Column data frame.
+  col_names <- paste("marginal_effect", 1:ncol(marg_effects_df))
   colnames(marg_effects_df) <- col_names  # Assign the column names to the data frame
   
   return(marg_effects_df)  # Return the data frame of marginal effects
 }
+
+
+# Calculating Marginal effects for the ZIOP model.
+ziop_marginal_effects(first_ZIOP_model)
