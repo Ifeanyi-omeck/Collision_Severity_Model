fde80c7c3a26d7afe5beff91ee0a42b8aaec8f6a
Ifeanyi-omeck <kifeanyi66@gmail.com>
2023-08-17 23:43:35 +0100

    making changes to code

---
+++ Marginal_effect_ordered.R
@@ -0,0 +1,140 @@
+marginal_effect_ordered <- function(ZIOP_MODEL, data, rho) {
+  
+  response_variable <- length(unique(ZIOP_MODEL$y))
+  
+  # Extract the ordered Probit coefficients from the ZIOP model
+  ordered_estimates <- unname(ZIOP_MODEL$coefficients$ordered)
+  
+  # Extract the inflation coefficients from the ZIOP model, excluding the intercept
+  infl_estimates <- unname(ZIOP_MODEL$coefficients$inflation[-1])
+  
+  intercept_term <- unname(ZIOP_MODEL$coefficients$inflation[1])
+  
+  # Extracting coefficient common to both part of the model
+  #unique_val <- c(Gamma, Beta)
+  
+  # xi represents the variables used in the ordered component of the ZIOP model
+  x_variables <- as.matrix(data[, colnames(ZIOP_MODEL$x)])
+  
+  col_names_x <- colnames(ZIOP_MODEL$x)
+  
+  # Get column names from the ZIOP model's inflation variables
+  col_names_z <- colnames(ZIOP_MODEL$z)
+  
+  # Clean the column names by removing the 'inflation.' prefix
+  col_names_z <- gsub("inflation.", "", col_names_z)
+  
+  # Remove the first element from the column names vector, which is the intercept term
+  col_names_z <- col_names_z[-1]
+  
+  # Extract inflation variables from the data using the cleaned column names and convert to a matrix
+  z_variables <- as.matrix(data[, col_names_z])
+  
+  # Extracting the levels of severity in the in the model
+  J_levels_ordered <- unique(ZIOP_MODEL$y) 
+  
+  # Create the formula for the model ordered section of the model
+  form_x  <- paste("~ 1", paste("+", col_names_x, collapse = " "), collapse = " ") 
+  
+  # Create the formula for the Binary Probit section of the model
+  form_z  <- paste("~ 1", paste("+", col_names_z, collapse = " "), collapse = " ") 
+  
+  # Generate the design matrix for the model. Excluding the intercept.
+  x_lin <- model.matrix(as.formula(form_x), data = data)[, -1]
+  
+  z_lin <- model.matrix(as.formula(form_z), data = data)[, -1]
+  
+  # since Marginal effects could be  calculated at the mean of the independent variables(joint mean of the covariates)
+  # The mean of independent variables for both the Binary and Ordered probit are calculated.
+  # The Author chose to calculate the Marginal effect at the join mean of the covariates below.
+  x_mean <- as.matrix(colMeans(x_lin))
+  Y_mean <- as.matrix(colMeans(z_lin))
+  
+  # Calculate XB and zY by multiplying the mean and model estimates
+  xb <- t(x_mean) %*% ordered_estimates
+  zY <- t(Y_mean) %*% infl_estimates
+  
+  # assign  X* and Y* to a variable. They include coefficients associated not only with the variables included 
+  # in either Binary or Ordered probit but also the common variables that appear in both equations.
+  # Extracting coefficients
+  ordered_coefs <- ZIOP_MODEL$coefficients$ordered
+  inflation_coefs <- ZIOP_MODEL$coefficients$inflation[-1]
+  
+  # Finding unique and common variable names
+  common_vars_names <- intersect(names(ordered_coefs), names(inflation_coefs))
+  unique_ordered_vars_names <- setdiff(names(ordered_coefs), common_vars_names)
+  unique_inflation_vars_names <- setdiff(names(inflation_coefs), common_vars_names)
+  
+  # Populate x_asteric and Y_asteric lists based on the unique and common variables
+  x_asteric <- list()
+  Y_asteric <- list()
+  
+  # Looping through the variables to find unique or common variables for x* AND  Y*
+  if (length(unique_ordered_vars_names) > 0 || length(unique_inflation_vars_names) > 0) {
+    # For ordered_coefs
+    x_asteric[common_vars_names] <- ordered_coefs[common_vars_names]
+    x_asteric[unique_ordered_vars_names] <- ordered_coefs[unique_ordered_vars_names]
+    
+    # For inflation_coefs
+    Y_asteric[common_vars_names] <- inflation_coefs[common_vars_names]
+    Y_asteric[unique_inflation_vars_names] <- inflation_coefs[unique_inflation_vars_names]
+  } else {
+    x_asteric[common_vars_names] <- ordered_coefs[common_vars_names]
+    Y_asteric[common_vars_names] <- inflation_coefs[common_vars_names]
+  }
+  
+  # Removing the inflation.name(ie. inflation.age hence making it just age)
+  names(Y_asteric) <- gsub("inflation.", "", names(Y_asteric))
+  
+  #Converting list to Matix for allow for multiplication and transposing them
+  x_asteric_mat <- as.matrix(unlist(x_asteric))
+  x_asteric_mat <- t(x_asteric_mat)
+  
+  Y_asteric_mat <- as.matrix(unlist(Y_asteric))
+  Y_asteric_mat  <- t(Y_asteric_mat)
+  
+  # Extract the cut-points (threshold parameters) from the ZIOP model
+  alpha <-  summary(ZIOP_MODEL)$coefficients[grep("^cut", rownames(summary(ZIOP_MODEL)$coefficients)), "Estimate"]  
+  alpha <- c(-10^6, alpha, 10^6)  # Set lower and upper bounds for the cut-points
+  
+  # Calculating the Marginal effects for the Zero inflated Ordered Probit Model
+  binary_prob <- (pnorm((alpha[2:(response_variable + 1)] - c(xb) + c(rho * zY)) / sqrt(1 - rho^2)) - 
+                    pnorm((alpha[1:response_variable] - c(xb) + c(rho * zY)) / sqrt(1 - rho^2))) * dnorm(c(zY))
+  
+  ordered_prob <- pnorm((c(zY + rho) * (alpha[1:response_variable] -c(xb))) / sqrt(1 - rho^2)) * dnorm(alpha[1:response_variable] - c(xb)) - 
+    pnorm((c(zY + rho) * (alpha[2:(response_variable + 1)] - c(xb))) / sqrt(1 - rho^2))  * dnorm(alpha[2:(response_variable + 1)] - c(xb))
+  
+  # Multiply by X* and Y* which represents coefficients associated not only with the variables included 
+  # in either Binary or Ordered probit but also the common variables that appear in both equations
+  binary_probit_effect <- as.vector(Y_asteric_mat) %*% matrix(data =  binary_prob, nrow = 1) 
+  ordered_probit_effect <- as.vector(x_asteric_mat) %*% matrix(data = ordered_prob, nrow = 1)
+  
+  # Add the Marginal effect for both term one and term two
+  overall_marginal_effect <- binary_probit_effect + ordered_probit_effect
+  
+  # Assign the row names and column to the binary and Ordered probit section of the returned data.,
+  # Passing variables to a data frame.
+  binary_probit_effect <- as.data.frame(binary_probit_effect)
+  ordered_probit_effect<- as.data.frame(ordered_probit_effect)
+  overall_marginal_effect <- as.data.frame(overall_marginal_effect)
+  
+  row.names(binary_probit_effect) <- col_names_z
+  row.names(ordered_probit_effect) <- col_names_x
+  
+  # renaming the columns for the Ordered probit section.
+  colnames(ordered_probit_effect)[colnames(ordered_probit_effect) %in% c("V1", "V2", "V3")] <- c("Minor Injury", 
+                                                                                                 "Serious Injury", "Fatal Injury")
+  
+  # Return various Marginal effect component
+  return(list(Binary_Probit = binary_probit_effect, Ordered_Probit = ordered_probit_effect, 
+              Overall_effect = overall_marginal_effect))
+}
+
+
+
+# Example usage : Here Rho is set to zero since the ZIOP model assumes independence among the latent processes.
+# For correlated disturbance include the Rho value which is normally within (-1 and 1).
+effects <- marginal_effect_ordered(ziop_two, prep_data, rho = 0)
+effects$Ordered_Probit
+
+summary(ziop_two)
---
+++ ZIOP_daylight.R
@@ -0,0 +1,188 @@
+# Install the "devtools" package if not already installed.
+if (!requireNamespace("devtools", quietly = TRUE)) {
+  install.packages("devtools")
+}
+
+# Install the "Ordinal" Package if not already installed.
+if(!require(ordinal)) {
+  install.packages("ordinal")
+  library(ordinal)
+}
+
+# Load the required packages for Zero inflated Ordered Probit model and the Ordinal Model.
+library(zmiop)
+library(ordinal)
+
+# Set a seed for reproducibility
+seed_value <- 123
+set.seed(seed_value)
+
+
+# Fitting model for the first iteration of the zero inflated ordered probit model for Day light.
+# Load the prepared data set for modelling.
+prep_data <- read.csv('final_dataset_day_light.csv')
+
+
+# Fitting the first Traditional ordered Probit model using the CLM function in the ordinal package.
+Op_one <- clm(factor(recoded_severity) ~ road_type +  speed_limit + weather_conditions + road_surface_conditions +
+                                         special_conditions_at_site + carriageway_hazards + urban_or_rural_area + vehicle_type +
+                                         skidding_and_overturning + first_point_of_impact + journey_purpose_of_driver + sex_of_driver +
+                                         age_of_driver + age_of_vehicle + sex_of_casualty + age_of_casualty + car_passenger,
+                                         data = prep_data, link = 'probit')
+
+summary(Op_one)
+
+# Fitting Ordered Probit model with Interaction terms between sex of driver and speed limit
+Op_two <- clm(factor(recoded_severity) ~ road_type +  speed_limit  + weather_conditions + road_surface_conditions +
+                                         special_conditions_at_site + carriageway_hazards + urban_or_rural_area + vehicle_type +
+                                         skidding_and_overturning + first_point_of_impact + journey_purpose_of_driver + sex_of_driver +
+                                         age_of_driver + age_of_vehicle + sex_of_casualty + age_of_casualty + car_passenger +
+                                         sex_of_driver:speed_limit, data = prep_data, link = 'probit')
+
+summary(Op_two)
+
+
+
+
+# Fitting the Ordered Probit model with interaction terms between weather conditions and road surface conditions
+Op_three <- clm(factor(recoded_severity) ~ road_type +  speed_limit +  weather_conditions + road_surface_conditions +
+                                         special_conditions_at_site + carriageway_hazards + urban_or_rural_area + vehicle_type +
+                                         skidding_and_overturning + first_point_of_impact + journey_purpose_of_driver + sex_of_driver +
+                                         age_of_driver + age_of_vehicle + sex_of_casualty + age_of_casualty + car_passenger +
+                                         weather_conditions:road_surface_conditions, data = prep_data, link = 'probit')
+
+
+summary(Op_three)
+
+
+# Fitting the Zero inflated Ordered Probit model with significant estimates from the Ordered Probit Model
+ziop_one_daylight <- iop(recoded_severity ~  road_type + speed_limit + weather_conditions + road_surface_conditions + 
+                                             special_conditions_at_site + carriageway_hazards + urban_or_rural_area +
+                                             vehicle_type + skidding_and_overturning + first_point_of_impact +
+                                             journey_purpose_of_driver + sex_of_driver + age_of_driver + age_of_vehicle + 
+                                             sex_of_casualty + age_of_casualty + car_passenger | road_type + speed_limit + 
+                                             weather_conditions + road_surface_conditions + special_conditions_at_site + 
+                                             carriageway_hazards + urban_or_rural_area + vehicle_type + skidding_and_overturning + 
+                                             first_point_of_impact + journey_purpose_of_driver + sex_of_driver + age_of_driver + 
+                                             age_of_vehicle +  sex_of_casualty + age_of_casualty + car_passenger,
+                                             data = prep_data, type = c('ziop'))
+
+
+summary(ziop_one_daylight)
+
+
+# Compare statistically significant variables in the OP model and ZIOP model, pick statistically significant variables
+# in either model and common to both models
+# fitting the model again with statistically significant variables 
+ziop_two_daylight <- iop(recoded_severity ~ road_type +  speed_limit + weather_conditions + carriageway_hazards + 
+                                            vehicle_type + skidding_and_overturning + first_point_of_impact  +
+                                            journey_purpose_of_driver + sex_of_driver + age_of_casualty + car_passenger |
+                                            road_type + speed_limit + weather_conditions + carriageway_hazards  +
+                                            vehicle_type + skidding_and_overturning + first_point_of_impact  + 
+                                            journey_purpose_of_driver + sex_of_driver + age_of_casualty + car_passenger,
+                                            data = prep_data, type = c('ziop'))
+
+summary(ziop_two_daylight)
+
+
+# Fit a standard ordered probit model with the same variables for comparison
+final_op_daylight <- clm(factor(recoded_severity) ~ road_type + speed_limit + weather_conditions + carriageway_hazards +
+                                       vehicle_type + skidding_and_overturning + first_point_of_impact + 
+                                       journey_purpose_of_driver + sex_of_driver + age_of_casualty + car_passenger,
+                                       data = prep_data, link = 'probit')
+
+
+summary(final_op_daylight)
+
+
+# Saving the results of the Zero inflated Ordered Probit to a csv file
+ziop_model <- summary(ziop_two_daylight)$coefficients
+ziop_model <- as.data.frame(ziop_model)
+ziop_model
+
+# Navigate to the 'Results' folder within the current working directory
+setwd("Results")
+write.csv(ziop_model, 'Estimates_ZIOP_daylight.csv', row.names = FALSE)
+
+
+# Saving the results of the Ordered Probit model to a csv file.
+Op_final_results <-  final_op_daylight$coefficients
+Op_final_results <- as.data.frame(Op_final_results)
+write.csv(Op_final_results, 'Estimates_OP_daylight.csv', row.names = FALSE)
+
+###################################################################################################################################################
+# Calculate AIC BIC and Log likelihood for the Ordered Probit model and them to csv
+compute_AIC_BIC_LL <- function(model, file_suffix) {
+  aic <- AIC(model)
+  print(paste("AIC:", aic))
+  
+  bic <- BIC(model)
+  print(paste("BIC:", bic))
+  
+  logLik_value <- logLik(model)
+  print(paste("Log-Likelihood:", logLik_value))
+  
+  # Create data frames for AIC, BIC, and log-likelihood values
+  aic_df <- data.frame(AIC = aic)
+  bic_df <- data.frame(BIC = bic)
+  log_likelihood_df <- data.frame(LogLikelihood = logLik_value)
+  
+  # Save the data frames to separate CSV files with the specified file_suffix
+  write.csv(aic_df, file = paste0("AIC_", file_suffix, ".csv"), row.names = FALSE)
+  write.csv(bic_df, file = paste0("BIC_", file_suffix, ".csv"), row.names = FALSE)
+  write.csv(log_likelihood_df, file = paste0("LogLikelihood_", file_suffix, ".csv"), row.names = FALSE)
+}
+
+# Return values for the ordered Probit model for comparison
+compute_AIC_BIC_LL(final_op_daylight, "Op_model_daylight")
+
+
+# Calculate AIC, BIC and Log Likelihood for the zero inflated Ordered Probit Model(ZIOP Model)
+calculate_AIC_BIC_LL_ZIOP <- function(model, n ){
+
+  
+# Run the function for calculating AIC and BIC first before running this function
+estimates <- calculate_AIC_BIC(model = model , n = n)  # Custom code that calculates AIC and BIC for the ZIOP MODEL
+ZIOP_AIC <- estimates$AIC
+print(paste("AIC:",  ZIOP_AIC))
+
+ZIOP_BIC <- estimates$BIC
+ZIOP_BIC
+print(paste("BIC:",  ZIOP_BIC))
+
+
+ZIOP_Log_likelihood <- model$ll
+ZIOP_Log_likelihood 
+print(paste("Likelihood:",  ZIOP_Log_likelihood))
+
+
+# Create data frames for AIC, BIC, and log-likelihood values
+aic_df_ziop <- data.frame(AIC = ZIOP_AIC)
+bic_df_ziop <- data.frame(BIC = ZIOP_BIC)
+log_likelihood_df_ziop <- data.frame(LogLikelihood = ZIOP_Log_likelihood)
+
+# Save the data frames to separate CSV files with the specified file_suffix
+write.csv(aic_df_ziop, file = paste0("AIC_", "Ziop_model_daylight", ".csv"), row.names = FALSE)
+write.csv(bic_df_ziop, file = paste0("BIC_", "Ziop_model_daylight", ".csv"), row.names = FALSE)
+write.csv(log_likelihood_df_ziop, file = paste0("LogLikelihood_daylight_", "Ziop_model", ".csv"), row.names = FALSE)
+}
+
+
+# Return values for Zero inflated Ordered Probit model for Comparison
+calculate_AIC_BIC_LL_ZIOP(ziop_two_daylight, 10712)
+
+####################################################################################################################################################
+# Calculating the Vuong test and Marginal effect for the Zero inflated Ordered Probit Model.
+# The Vuong test score between the Ordered Probit model and the Zero Inflated Ordered Probit Model.
+# Storing the results in a csv file 
+voung_result <- vuong_test(final_op_daylight, ziop_two_daylight, prep_data) # A custom code that calculates the Vuong test.
+voung_result_df <- data.frame(voung_result)
+write.csv(voung_result_df, 'vuong_test_result_daylight.csv', row.names = FALSE)
+voung_result
+
+
+# Marginal effect for the Zero inflated Ordered Probit Model
+ziop_marginal_result <- marginal_effect_ordered(ziop_two_daylight, prep_data, rho = 0) # A custom code for Calculating Marginal effect.
+ziop_marginal_result_df <- data.frame(ziop_marginal_result$Ordered_Probit)
+write.csv(ziop_marginal_result_df, 'ziop_marginal_result_daylight.csv', row.names = FALSE)
+ziop_marginal_result_df
